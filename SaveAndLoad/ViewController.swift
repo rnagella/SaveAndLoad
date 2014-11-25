b4bbe106cdd6a28a68478e7abd30e1ca7f2573a8
Ranjith Nagella <nagella.ranjith@gmail.com>
2014-11-24 22:23:12 -0500

    Helper function to create a file path.

--- SaveAndLoad/ViewController.swift
+++ SaveAndLoad/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     // Document Directory /User/rnagella/.../Documnets
     // Users/rknage/Library/Developer/CoreSimulator/Devices/0BD56009-288C-4E32-8F39-820ABE512DF6/data/Containers/Data/Application/0DDC3504-78C2-4EAC-AEE0-7413CA1B9238/Documents
-    // File in Document Directory /User/rnagella/../.Documents/Photo.png
+    
     
     func documentsDirectory() -> String{
         let documentFolderPath = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] as String
@@ -20,11 +20,22 @@ class ViewController: UIViewController {
         return documentFolderPath
     }
     
+    // File in Document Directory /User/rnagella/../.Documents/Photo.png
+    //    Photo Path: /Users/rknage/Library/Developer/CoreSimulator/Devices/0BD56009-288C-4E32-8F39-820ABE512DF6/data/Containers/Data/Application/7DCFFFDE-9810-4A6A-95E2-C37AD290EAA4/Documents/Photo.jpeg
+    
+    func fileInDocumentDirecory(filename: String) -> String {
+        
+        return documentsDirectory().stringByAppendingPathComponent(filename)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let documents = documentsDirectory()
         println("Documents:\(documents)")
+        
+        let imagePath = fileInDocumentDirecory("Photo.jpeg")
+        println("Photo Path: \(imagePath)")
     }
 
     override func didReceiveMemoryWarning() {
